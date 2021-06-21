f520ca26cba83a13d1f5abddf92522bd271e14aa
HaroldYontop <harold.yontop@soapros.pe>
2021-06-21 11:28:19 -0500

    agregando nueva etiqueta en ReceiptInfo para PC14

--- src/main/java/pe/soapros/bean/ReceiptInfo.java
+++ src/main/java/pe/soapros/bean/ReceiptInfo.java
@@ -2,18 +2,22 @@ package pe.soapros.bean;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import lombok.Data;
 
+@Data
 @JacksonXmlRootElement(localName="ReceiptInfo")
 public class ReceiptInfo {
 	@JsonProperty("LastReceiptDueDate")
 	private String lastReceiptDueDate;
+	@JsonProperty("LastReceiptCoverageEndDate")
+	private String lastReceiptCoverageEndDate;
 
-	public String getLastReceiptDueDate() {
-		return lastReceiptDueDate;
-	}
+	//public String getLastReceiptDueDate() {
+	//	return lastReceiptDueDate;
+	//}
 
-	public void setLastReceiptDueDate(String lastReceiptDueDate) {
-		this.lastReceiptDueDate = lastReceiptDueDate;
-	}
+	//public void setLastReceiptDueDate(String lastReceiptDueDate) {
+	//	this.lastReceiptDueDate = lastReceiptDueDate;
+	//}
 	
 }
