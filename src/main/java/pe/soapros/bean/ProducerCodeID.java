e6dbfa889936da0ff00208723bf4f176aab2776a
HaroldYontop <harold.yontop@soapros.pe>
2021-06-28 10:14:55 -0500

    Declaracion de los atributos ProducerCodeId.Code y Role.Code como array para PC14

--- src/main/java/pe/soapros/bean/ProducerCodeID.java
+++ src/main/java/pe/soapros/bean/ProducerCodeID.java
@@ -4,12 +4,14 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 @JacksonXmlRootElement(localName="ProducerCodeID")
 public class ProducerCodeID {
 
     @JsonProperty("Code")
-    private String code;
+    private List<String> code;
 
     @JsonProperty("Role")
     private Role role;
--- src/main/java/pe/soapros/bean/Role.java
+++ src/main/java/pe/soapros/bean/Role.java
@@ -4,10 +4,12 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 @JacksonXmlRootElement(localName="Role")
 public class Role {
 
     @JsonProperty("Code")
-    private String code;
+    private List<String> code;
 }
